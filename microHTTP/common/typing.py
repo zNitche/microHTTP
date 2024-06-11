12291cc1b9ecc000cca4520898e85e1dcad16fcf
zNitche <53448802+zNitche@users.noreply.github.com>
2024-06-11 23:12:30 +0200

    added common files

---
+++ microHTTP/common/typing.py
@@ -0,0 +1,4 @@
+# Due to lack typing module for micropython
+# I figured this would be a better workaround than using 'if False:' while importing type checking components
+
+TYPE_CHECKING = False
---
+++ microHTTP/common/utils.py
@@ -0,0 +1,33 @@
+import sys
+
+from microHTTP.common.typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from asyncio import StreamReader
+
+
+def print_debug(message: str, debug_enabled: bool = False, exception: Exception | None = None):
+    if debug_enabled:
+        print(f"[microHTTP] - {message}")
+
+        if exception and isinstance(exception, Exception):
+            sys.print_exception(exception)
+
+
+async def load_request_header_from_stream(stream: StreamReader) -> str:
+    request_header_string = ""
+
+    while True:
+        request_line = await stream.readline()
+        request_line = request_line.decode()
+
+        if request_line == "\r\n" or not request_line:
+            break
+
+        request_header_string += request_line
+
+    return request_header_string
+
+
+def write_to_stream(stream, data, encoding="utf-8"):
+    stream.write(bytes(data, encoding))
